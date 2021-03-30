2dea351fc93af7c2e0751bbd2ab0b115811e13dd
klueless-io <david@ideasmen.com.au>
2021-03-30 12:34:34 +1100

    first commit

---
+++ lib/k_doc.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "k_doc/version"
+
+module KDoc
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/k_doc_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe KDoc do
+  it "has a version number" do
+    expect(KDoc::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
